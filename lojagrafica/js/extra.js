fd0fe1acce4b1eca14992ce34ee02c68fd820cd7
adrianoOliveiraRocha <adriano.qwe32@gmail.com>
2018-05-24 14:57:06 -0300

    extra.js changed

--- lojagrafica/js/extra.js
+++ lojagrafica/js/extra.js
@@ -3,7 +3,14 @@ function delivery(order_id, opt){
 	window.self.location.href = url;
 }
 
-// function approved(orderItemArt_id, approved){
-// 	url = "/checkout/approved/" + orderItemArt_id + '/' + approved;
-// 	window.self.location.href = url;
-// }
+function solicitation(opt){
+	let element = document.getElementById("solicitation").style.visibility = "visible" ;
+	if (opt === 1) {
+		document.getElementById("solicitation").style.visibility = "visible" ;
+		document.getElementById("btn_solicitation").style.visibility = "hidden" ;
+	} else {
+		document.getElementById("solicitation").style.visibility = "hidden";
+		document.getElementById("btn_solicitation").style.visibility = "visible";
+	}
+}
+
